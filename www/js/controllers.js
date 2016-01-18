cc555281c34ece57cec7c6e89ff9fc0bb64bef38
Jiazhen Xiong <Jiazhen Xiong>
2016-01-18 22:03:35 +0800

    计划--药物

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7778,49 +7778,85 @@ $scope.$on('RisksGet',function(){
 
         // console.log($rootScope.TempList.AddList);
         // console.log($rootScope.TempList.DeleteList);
+        var TEAdd=[],TEDel=[];
+        for(var i=$rootScope.TempList.AddList.length-1;i>=0;i--){
+          if($rootScope.TempList.AddList[i].Type=='TE'){
+            TEAdd.push($rootScope.TempList.AddList[i]);
+            $rootScope.TempList.AddList.splice(i,1);
+          }
+        }
+        for(var i=$rootScope.TempList.DeleteList.length-1;i>=0;i--){
+          if($rootScope.TempList.DeleteList[i].Type=='TE'){
+            TEDel.push($rootScope.TempList.DeleteList[i]);
+            $rootScope.TempList.DeleteList.splice(i,1);
+          }
+        }   
         if($rootScope.TempList.AddList.length>0){
           // console.log("有添加")
           PlanInfo.SetTask($rootScope.TempList.AddList).then(function(data){
-              if(data.result=='数据插入成功'){
-                // console.log("添加")
-                    $rootScope.TempList.AddList.length = 0;
-
-                    if($rootScope.TempList.DeleteList.length>0){
-                      // console.log("有添加又有删除")
-                      PlanInfo.DeleteTask($rootScope.TempList.DeleteList).then(function(data){
-                        if(data.result=='数据删除成功'){
-                          // console.log("删除")
-                             $rootScope.TempList.DeleteList.length = 0;
-                             $ionicHistory.goBack();
-                        }
-                      })
-                    }
-
-                    else {
-                       // console.log("有添加没有删除")
-                      $ionicHistory.goBack(); 
-                    }
-              }
-          })
-        }
-        else{
-            if($rootScope.TempList.DeleteList.length>0){
-              // console.log("只有删除")
-              PlanInfo.DeleteTask($rootScope.TempList.DeleteList).then(function(data){
-                if(data.result=='数据删除成功'){
-                    // console.log("deleted")
+            if(data.result=='数据插入成功'){
+              // console.log("添加")
+              $rootScope.TempList.AddList.length = 0;
+              if($rootScope.TempList.DeleteList.length>0){
+                // console.log("有添加又有删除")
+                PlanInfo.DeleteTask($rootScope.TempList.DeleteList).then(function(data){
+                  if(data.result=='数据删除成功'){
+                    // console.log("删除")
                     $rootScope.TempList.DeleteList.length = 0;
-                    $ionicHistory.goBack();                      
-                }
-              })
+                    $ionicHistory.goBack();
+                  }
+                })
+              }else{
+                // console.log("有添加没有删除")
+                $ionicHistory.goBack(); 
+              }
             }
-
-          else {
+          })
+        }else{
+          if($rootScope.TempList.DeleteList.length>0){
+            // console.log("只有删除")
+            PlanInfo.DeleteTask($rootScope.TempList.DeleteList).then(function(data){
+              if(data.result=='数据删除成功'){
+                // console.log("deleted")
+                $rootScope.TempList.DeleteList.length = 0;
+                $ionicHistory.goBack();                      
+              }
+            })
+          }else{
             // console.log("啥也没有")
             $ionicHistory.goBack();
           }
+        }          
+        $rootScope.TempList.AddList=TEAdd;
+        $rootScope.TempList.DeleteList = TEDel;
+        if($rootScope.TempList.DeleteList.length>0){
+          PlanInfo.DeleteTask($rootScope.TempList.DeleteList).then(function(data){
+            if(data.result=='数据删除成功'){
+              $rootScope.TempList.DeleteList.length = 0;
+              if($rootScope.TempList.AddList.length>0){
+                PlanInfo.SetTask($rootScope.TempList.AddList).then(function(data){
+                  if(data.result=='数据插入成功'){
+                    $rootScope.TempList.AddList.length = 0;
+                    $ionicHistory.goBack();
+                  }
+                })
+              }else{
+                $ionicHistory.goBack(); 
+              }
+            }
+          })
+        }else{
+          if($rootScope.TempList.AddList.length>0){
+            PlanInfo.SetTask($rootScope.TempList.AddList).then(function(data){
+              if(data.result=='数据插入成功'){
+                $rootScope.TempList.AddList.length = 0;
+                $ionicHistory.goBack();                      
+              }
+            })
+          }else{
+            $ionicHistory.goBack();
+          }
         }        
-        
     }
 }])
 
@@ -9077,7 +9113,6 @@ $scope.$on('RisksGet',function(){
     if(!$rootScope.TempList.AddList) $rootScope.TempList.AddList = new Array();
     if(!$rootScope.TempList.DeleteList) $rootScope.TempList.DeleteList = new Array();
     $scope.isloaded = false;
-
     //loading图标显示
     $ionicLoading.show({
         content: '加载中',
@@ -9100,7 +9135,24 @@ $scope.$on('RisksGet',function(){
     {
         var promise = PlanInfo.GetTasks(PlanNo, Type + "0000");  
         promise.then(function(data) { 
-            //console.log(data);
+            console.log(data);
+            var decorate = function (dataobj)
+            {
+                dataobj.Description = dataobj.Instruction.split(':')[0];
+                dataobj.OptionCategory = "";
+                var arry = dataobj.Instruction.split(':')[1].split('|');
+                for (var j = 0; j < arry.length; j++)
+                {
+                    if (arry[j] != "")
+                    {
+                        dataobj.OptionCategory += TypeList[j] + arry[j];
+                    }
+                }
+                if (dataobj.OptionCategory != "")
+                {
+                    dataobj.OptionCategory = dataobj.OptionCategory.substring(1);
+                }
+            }
             for (var i=0; i<data.length; i++)
             {   
                 if(data[i].Instruction == "")
@@ -9109,28 +9161,40 @@ $scope.$on('RisksGet',function(){
                 }
                 else
                 {
-                    data[i].Description = data[i].Instruction.split(':')[0];
-                    data[i].OptionCategory = "";
-                    var arry = data[i].Instruction.split(':')[1].split('|');
-                    for (var j = 0; j < arry.length; j++)
-                    {
-                        if (arry[j] != "")
-                        {
-                            data[i].OptionCategory += TypeList[j] + arry[j];
-                        }
-                    }
-                    if (data[i].OptionCategory != "")
-                    {
-                        data[i].OptionCategory = data[i].OptionCategory.substring(1);
-                    }
+                    // data[i].Description = data[i].Instruction.split(':')[0];
+                    // data[i].OptionCategory = "";
+                    // var arry = data[i].Instruction.split(':')[1].split('|');
+                    // for (var j = 0; j < arry.length; j++)
+                    // {
+                    //     if (arry[j] != "")
+                    //     {
+                    //         data[i].OptionCategory += TypeList[j] + arry[j];
+                    //     }
+                    // }
+                    // if (data[i].OptionCategory != "")
+                    // {
+                    //     data[i].OptionCategory = data[i].OptionCategory.substring(1);
+                    // }
                     $scope.task.DeleteList.push({"PlanNo":PlanNo, "Type":Type, "Code":Type + "0001", "SortNo":(i+1).toString()});
                 }
-            }                      
-            $scope.task.list = data;
+            } 
+            var matchkey = {"Type":'TE'};
+            var t = $rootScope.TempList.AddList.indexOf(matchkey);
+            if(t!=-1){
+              data=$rootScope.TempList.AddList.slice(1);
+            }
+            console.log(data)
+            console.log($rootScope.TempList.AddList)
+            console.log($rootScope.TempList.DeleteList)
+            if(data.length>0){
+              for(var i=0 ; i<data.length;i++)
+              {
+                decorate(data[i]);
+              } 
+              $scope.task.list = data;                        
+            }
             $ionicLoading.hide();
             $scope.isloaded = true;
-            ////console.log($scope.task.list);
-            ////console.log($scope.task.DeleteList);
         }, function(data) {  
         });   
     }
@@ -9332,14 +9396,15 @@ $scope.$on('RisksGet',function(){
 
     $scope.Confirm = function ()
     {
-        var AddList = new Array();
+        var myAddList = new Array();
+        console.log($scope.task.list.length)
         if ($scope.task.list.length > 0)
         {
-            AddList.push({"PlanNo":PlanNo, "Type":Type, "Code":Type + "0000", "SortNo":'1', "Instruction":"", "piUserId":"1",  "piTerminalName":"1",  "piTerminalIP":"1",  "piDeviceType":0}); //父级条目
+            myAddList.push({"PlanNo":PlanNo, "Type":Type, "Code":Type + "0000", "SortNo":'1', "Instruction":"", "piUserId":"1",  "piTerminalName":"1",  "piTerminalIP":"1",  "piDeviceType":0}); //父级条目
         }
         for(var i=0; i< $scope.task.list.length; i++)
         {
-            AddList.push({"PlanNo":PlanNo, "Type":Type, "Code":Type + "0001", "SortNo":(i+1).toString(), "Instruction":$scope.task.list[i].Instruction, "piUserId":"1",  "piTerminalName":"1",  "piTerminalIP":"1",  "piDeviceType":0});
+            myAddList.push({"PlanNo":PlanNo, "Type":Type, "Code":Type + "0001", "SortNo":(i+1).toString(), "Instruction":$scope.task.list[i].Instruction, "piUserId":"1",  "piTerminalName":"1",  "piTerminalIP":"1",  "piDeviceType":0});
         }
         if ($scope.task.DeleteList.length > 0)
         {
@@ -9350,21 +9415,10 @@ $scope.$on('RisksGet',function(){
             }
             else
             {
-                 var promise = PlanInfo.DeleteTask($scope.task.DeleteList);
-                  promise.then(function(data) 
-                  {
-                      if (data.result == "数据删除成功")
-                      {
-                          SetTask(AddList);
-                      }
-                  },function(data){
-                  });  
+                DeleteTask($scope.task.DeleteList); 
             }
         }
-        else
-        {
-            SetTask(AddList);
-        }
+        SetTask(myAddList);
     }
 
     function SetTask(obj)
@@ -9378,13 +9432,19 @@ $scope.$on('RisksGet',function(){
         //         $ionicHistory.goBack();
         //     }
         // },function(data){              
-        // });   
-
-        for (var i = obj.length - 1; i >= 0; i--) {
-          if($rootScope.TempList.AddList.indexOf(obj[i]) == -1 )
-               $rootScope.TempList.AddList.push(obj[i]);
-        };
-       
+        // });
+        console.log($rootScope.TempList.AddList)
+        for(var i=$rootScope.TempList.AddList.length-1;i>=0;i--){
+          if($rootScope.TempList.AddList[i].Type=="TE"){
+            $rootScope.TempList.AddList.splice(i,1);
+          }
+        }
+        console.log($rootScope.TempList.AddList)
+        if(obj.length>0){
+          for(var i in obj){
+            $rootScope.TempList.AddList.push(obj[i]);
+          }        
+        }
         $ionicHistory.goBack();
     }
 
@@ -9398,12 +9458,17 @@ $scope.$on('RisksGet',function(){
         //         $ionicHistory.goBack();
         //     }
         // },function(data){
-        // });   
-        for (var i = obj.length - 1; i >= 0; i--) {
-          if($rootScope.TempList.DeleteList.indexOf(obj[i]) == -1 )
-               $rootScope.TempList.DeleteList.push(obj[i]);
-        };
-
+        // });
+        for(var i=$rootScope.TempList.DeleteList.length-1;i>=0;i--){
+          if($rootScope.TempList.DeleteList[i].Type=="TE"){
+            $rootScope.TempList.DeleteList.splice(i,1);
+          }
+        }          
+        if(obj.length>0){
+          for(var i in obj){
+            $rootScope.TempList.DeleteList.push(obj[i]);
+          }        
+        } 
         $ionicHistory.goBack();
     }
 
